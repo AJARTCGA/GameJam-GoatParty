e74e6fccfd172eb30f4740d5f8fea721f1de6f52
AJARTCGA <ajartcga@gmail.com>
2017-09-22 19:04:03 -0400

    Uploading a base LibGDX project

---
+++ html/src/com/nastyrowdyboys/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.nastyrowdyboys.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.nastyrowdyboys.game.GameJamGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new GameJamGame();
+        }
+}
